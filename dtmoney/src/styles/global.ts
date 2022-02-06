3d756b639afa5b5d59a4549c617a87482161fa4a
Adilson Ramos <75184157+AdilsonRamosO@users.noreply.github.com>
2022-02-06 14:25:49 -0300

    TransactionsTable

--- dtmoney/src/styles/global.ts
+++ dtmoney/src/styles/global.ts
@@ -9,7 +9,7 @@ export const GlobalStyle = createGlobalStyle`
         --blue-light: #6933FF;
         --text-title: #363f5f;
         --text-body: #969cb3;
-        --shape: #FFFFFF;
+        --shape: #fff;
 
     }
 
